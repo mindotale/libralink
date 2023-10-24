4ddf74fe5e60a9d2089d00129fc8e7063732d4d0
Volodymyr Domin <mindotale@gmail.com>
2023-10-24 23:33:21 +0300

    style: Apply formatting

--- tests/Domain.Tests.Unit/UnitTest1.cs
+++ tests/Domain.Tests.Unit/UnitTest1.cs
@@ -1,11 +1,9 @@
-namespace Domain.Tests.Unit
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+namespace Libralink.Domain.Tests.Unit;
 
-        }
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
     }
 }
