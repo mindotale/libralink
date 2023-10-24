a6860d5239ae8065e09a2169f797c7646480bda4
Valerii Inshyn <valeriyi.nshyn@gmail.com>
2023-10-25 00:38:51 +0300

    feat: graphql initial setup

---
+++ src/Api/src/Domain/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libralink.Domain
+{
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public Author Author { get; set; }
+    }
+}
