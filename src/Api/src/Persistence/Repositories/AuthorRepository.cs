33c1a1a5c445d813a1e2e15bbf0fc4a7f11917d2
Volodymyr Domin <mindotale@gmail.com>
2023-10-28 00:01:32 +0300

    feat: Add repository implementations

---
+++ src/Api/src/Persistence/Repositories/AuthorRepository.cs
@@ -0,0 +1,51 @@
+﻿using Libralink.Application.Repositories;
+using Libralink.Domain;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Libralink.Persistence.Repositories;
+
+public sealed class AuthorRepository : IAuthorRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public AuthorRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Author?> GetAsync(Guid id)
+    {
+        return await _context.Authors.FindAsync(id);
+    }
+
+    public async Task<IList<Author>> GetAllAsync(int offset, int limit)
+    {
+        return await _context.Authors.Skip(offset).Take(limit).ToListAsync();
+    }
+
+    public async Task<Guid> AddAsync(Author author)
+    {
+        var entry = _context.Authors.Add(author);
+        await _context.SaveChangesAsync();
+        return entry.Entity.Id;
+    }
+
+    public async Task UpdateAsync(Author author)
+    {
+        var entry = _context.Authors.Update(author);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var author = await _context.Authors.FindAsync(id);
+        if (author is null)
+        {
+            throw new ArgumentException($"Author with id {id} not found.");
+        }
+
+        _context.Authors.Remove(author);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ src/Api/src/Persistence/Repositories/BookRepository.cs
@@ -0,0 +1,52 @@
+﻿using Libralink.Application.Repositories;
+using Libralink.Domain;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Libralink.Persistence.Repositories
+{
+    public sealed class BookRepository : IBookRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Book?> GetAsync(Guid id)
+        {
+            return await _context.Books.FindAsync(id);
+        }
+
+        public async Task<IList<Book>> GetAllAsync(int offset, int limit)
+        {
+            return await _context.Books.Skip(offset).Take(limit).ToListAsync();
+        }
+
+        public async Task<Guid> AddAsync(Book book)
+        {
+            var entry = _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+            return entry.Entity.Id;
+        }
+
+        public async Task UpdateAsync(Book book)
+        {
+            var entry = _context.Books.Update(book);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book is null)
+            {
+                throw new ArgumentException($"Book with id {id} not found.");
+            }
+
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
