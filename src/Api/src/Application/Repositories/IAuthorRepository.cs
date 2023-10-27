79fb6b373165691dd4e6dc55886a1bcaad3f6143
Volodymyr Domin <mindotale@gmail.com>
2023-10-27 23:48:40 +0300

    feat: Add repository interfaces

---
+++ src/Api/src/Application/Repositories/IAuthorRepository.cs
@@ -0,0 +1,16 @@
+﻿using Libralink.Domain;
+
+namespace Libralink.Application.Repositories;
+
+public interface IAuthorRepository
+{
+    public Task<Author?> GetAsync(Guid id);
+
+    public Task<IList<Author>> GetAllAsync(int offset, int limit);
+
+    public Task<Guid> AddAsync(Author author);
+
+    public Task UpdateAsync(Author author);
+
+    public Task DeleteAsync(Guid id);
+}
---
+++ src/Api/src/Application/Repositories/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using Libralink.Domain;
+
+namespace Libralink.Application.Repositories;
+
+public interface IBookRepository
+{
+    public Task<Book?> GetAsync(Guid id);
+
+    public Task<IList<Book>> GetAllAsync(int offset, int limit);
+
+    public Task<Guid> AddAsync(Book book);
+
+    public Task UpdateAsync(Book book);
+
+    public Task DeleteAsync(Guid id);
+}
